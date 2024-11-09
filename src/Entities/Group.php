9eeed0cea3d5cf80b2ea106af16ca42260c262dd
Bernhard Enders <bgeneto@users.noreply.github.com>
2024-11-09 14:48:17 -0300

    Update check for wildcard matching
    takes into consideration multiple permissions levels

--- src/Entities/Group.php
+++ src/Entities/Group.php
@@ -85,9 +85,16 @@ class Group extends Entity
         }
 
         // Check wildcard match
-        $check = substr($permission, 0, strpos($permission, '.')) . '.*';
+        $checks = [];
+        $parts = explode('.', $permission);
+        for ($i = count($parts); $i > 0; $i--) {
+            $check = implode('.', array_slice($parts, 0, $i)) . '.*';
+            $checks[] = $check;
+        }
 
-        return $this->permissions !== null && $this->permissions !== [] && in_array($check, $this->permissions, true);
+        return $this->permissions !== null && 
+               $this->permissions !== [] && 
+               array_intersect($checks, $this->permissions);
     }
 
     /**
